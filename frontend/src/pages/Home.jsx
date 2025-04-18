5611486b40083fd5137df7e191495c9ea58e0b4d
Gabriel França de Almeida <54248551+GabrielFAlmeida@users.noreply.github.com>
2025-04-18 19:16:56 -0300

    fix(feel.io): keep the relevant content in home component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,25 +1,35 @@
 import MoodCalendar from "../components/calendar/MoodCalendar";
-import LandBot from "../components/LandBot";
+import { motion } from "framer-motion";
+
+import "react-calendar/dist/Calendar.css";
+import "../index.css";
 
 export default function Home() {
   return (
-    <div className="w-full min-h-screen bg-white flex p-6 pt-20">
-      {/* Coluna da esquerda */}
-      <div className="w-2/3 relative flex justify-center items-start">
-        <div className="fixed top-0 left-0 w-full h-64 bg-gradient-to-b from-white to-transparent z-40 pointer-events-none" />
-
-        {/* Calendário */}
-        <MoodCalendar />
-      </div>
-
-      {/* Coluna da direita */}
-      <div className="w-1/3 pl-8 pr-4">
-      <LandBot />
-        <h2 className="text-xl font-semibold mb-4 text-gray-800">Escreva sobre seu dia</h2>
-        <textarea
-          placeholder="Hoje me senti..."
-          className="w-full h-64 p-4 border border-gray-300 rounded-lg resize-none shadow-sm focus:outline-none focus:ring-2 focus:ring-purple-500"
-        />
+    <div className="w-full bg-white flex">
+      <div className="min-h-screen bg-white dark:bg-gray-900 text-gray-900 dark:text-white transition duration-500">
+        <motion.main
+          className="flex flex-col items-center mt-10"
+          initial={{ opacity: 0, y: 30 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6 }}
+        >
+          <div className="bg-main-purple_200">
+            <h2 className="text-4xl  font-semibold mb-4 text-center">
+              Understand your emotions, whenever and wherever you are. Register
+              your mood daily and recieve personalized insights - let's do it
+              better together.
+            </h2>
+            <p className="text-center max-w-md text-gray-400 dark:text-gray-300">
+              Feel.io helps you track your mood and build emotional
+              self-awareness through simple check-ins. Feel free to share your
+              thoughts - this is your safe space.
+            </p>
+          </div>
+          <div className="max-w-sm">
+            <MoodCalendar />
+          </div>
+        </motion.main>
       </div>
     </div>
   );
